c40777b00eec7909d504347c62801f9a019f2cad
Chetan Manocha <109103528+cm-chetan16@users.noreply.github.com>
2023-03-04 23:05:09 +0530

    final

---
+++ src/constants.js
@@ -0,0 +1,64 @@
+const cardsData = [
+    {
+        sname:"LUCIFER",
+        imgsrc:'https://rukminim1.flixcart.com/image/416/416/kfwvcsw0-0/poster/g/a/a/large-lucifer-1-love-street-lucifer-tv-series-poster-netflix-tv-original-imafw9juqqybkpmz.jpeg?q=70',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80057918'
+    },
+    {
+        sname:"DARK",
+        imgsrc:"https://talenthouse-res.cloudinary.com/image/upload/c_fill,f_auto,h_905,w_640/v1591798269/user-1071618/submissions/syrlay4mfbbhvnow4uek.jpg",
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80100172'
+    },
+    {
+        sname:"MANIFEST",
+        imgsrc:"https://im.indiatimes.in/content/2022/Aug/EN-US_ManifestS4_4x5_RGB_PRE_630da1d70478e.jpg?w=640&h=799&cc=1",
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80241318'
+    },
+    {
+        sname:"MONEY HEIST",
+        imgsrc:'https://i.pinimg.com/originals/1d/4a/b8/1d4ab862150af42030bf029d5e2ad492.jpg',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80192098'
+    },
+    {
+        sname:"SQUID GAME",
+        imgsrc:'https://rukminim1.flixcart.com/image/416/416/kuh9yfk0/poster/d/p/f/extra-large-squid-game-netflix-poster-pack-of-2-posters-016-original-imag7hzzhavzgj6w.jpeg?q=70',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/81040344'
+    },
+    {
+        sname:"FRIENDS",
+        imgsrc:'https://rukminim1.flixcart.com/image/416/416/keuagsw0-0/poster/w/k/u/medium-friends02-friends-tv-series-poster-with-frame-friends-web-original-imafvfnyzx9gggqz.jpeg?q=70',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/70153404'
+    },
+    {
+        sname:"1899",
+        imgsrc:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQmCOOs9LsT5sXC06yteAl2I4GP2rC-b8jAQ6zXJ10&s',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80214497'
+    },
+    {
+        sname:"SACRED GAMES",
+        imgsrc:'https://www.adgully.com/img/800/201905/nawaz.jpg',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/80115328'
+    },
+    {
+        sname:"DELHI CRIME",
+        imgsrc:'https://deadline.com/wp-content/uploads/2022/08/delhi-crime-2.jpeg?w=729',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/81076756'
+    },
+    {
+        sname:"kOTA FACTORY",
+        imgsrc:'https://www.rvcj.com/wp-content/uploads/2019/05/kota-factory-poster.jpg',
+        title:'A Netflix Original Series',
+        links:'https://www.netflix.com/in/title/81249783'
+    },
+]
+
+export default cardsData
